93fff588abcb36c87b1e37f2aa456ca9d0ad3026
Benedicta Davour <benedictadavour777@gmail.com>
2025-02-16 18:24:04 +0000

    optimizeDeps for react

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react(),
     
   ],
-  build:{
-    rollupOptions:{
-      external:['react-slick']
-    }
+  optimizeDeps: {
+    include: ["react-slick"]
   }
 })
